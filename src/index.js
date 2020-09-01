0c4e134653cf3caf328056f48d832ae0b8a30fa5
Dallyla Oliveira <51199118+Dallyla@users.noreply.github.com>
2020-09-01 20:37:54 -0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <div>
+    <h1>My favorite foods</h1>
+    <ul>
+      <li>Sushi</li>
+      <li>Lasagna</li>
+      <li>Pizza</li>
+    </ul>
+  </div>,
+  document.getElementById("root")
+);
